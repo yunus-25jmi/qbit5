e4b2a28d6c278c3d5c7154acb73dbe85bb23b431
Yunus <91471219+yunus-25jmi@users.noreply.github.com>
2022-04-13 10:42:17 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
-curl -L $RCLONE_CONFIG_IN_RAW_URL >/root/.config/rclone/rclone.conf
+echo "Scripted By DevAnaZ"
+echo "Orgin repository : https://github.com/developeranaz/qbittorrent-to-rclone-heroku"
+mkdir /.config
+mkdir /.config/rclone
+curl -L $RCLONE_CONFIG_IN_RAW_URL >/.config/rclone/rclone.conf
 echo $PORT >/PORT
-curl -L $QBITTORRENT_CONFIG_IN_RAW_URL >/root/.config/qBittorrent/qBittorrent.conf
+#curl -L $QBITTORRENT_CONFIG_IN_RAW_URL
+cat /qBittorrent/config/qBittorrent.conf >/qBittorrent/qBittorrent.conf
+echo -n "$WEBPASSWORD" |md5sum |sed 's|  -||g' >/qBittorrent/WEBHASHPASSWORD
+cat /qBittorrent/qBittorrent.conf |sed "s|devanaz:|$(rclone listremotes |head -1)|g" |sed "s|developeranaz|$WEBUSER|g" |sed "s|bc89e64478e74b30bea280349e1220ce|$(cat /qBittorrent/WEBHASHPASSWORD)|g" >/qBittorrent/config/qBittorrent.conf
+
 while :
 do
-jupyter notebook --ip=0.0.0.0 --port="$(cat /PORT)" --NotebookApp.token='' --NotebookApp.password=''
-qbittorrent-nox --webui-port="$(cat /PORT)"
+qbittorrent-nox --profile=/ --webui-port="$(cat /PORT)"
 done
